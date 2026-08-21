18bba53575154f0974609929739e34ad960bd46e
Javier G. Grandez <jggrandez@gmail.com>
2026-08-21 12:45:33 +0200

    a vueltas con el banner

--- resources/js/banner.js
+++ resources/js/banner.js
@@ -1,33 +1,17 @@
-alert("banner.js cargado");
-
 document.addEventListener("DOMContentLoaded", () => {
-   alert("Antes del if");
   if (window.self !== window.top) {
-     alert("Es un iframe");
     const banner = document.getElementById("quarto-announcement");
+    if (banner) banner.style.display = "none";
+    
+    const link = document.querySelector('a[href$="#iasi-open-external"]');
+    if (link) {
+        link.classList.remove("iasi-hidden");
 
-    if (banner) {
-      banner.style.display = "none";
+        link.addEventListener("click", function (event) {
+             event.preventDefault();
+             window.open(window.location.href, "_blank", "noopener");
+        });
     }
   }
 });
 
-/*
-document.addEventListener("DOMContentLoaded", function () {
-  if (window.self === window.top) return;
-
-  const announcement = document.getElementById("quarto-announcement");
-  const link = document.querySelector('a[href$="#iasi-open-external"]');
-
-  if (announcement) announcement.classList.add("iasi-hidden");
-
-  if (link) {
-    link.classList.remove("iasi-hidden");
-
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      window.open(window.location.href, "_blank", "noopener");
-    });
-  }
-});
-*/
